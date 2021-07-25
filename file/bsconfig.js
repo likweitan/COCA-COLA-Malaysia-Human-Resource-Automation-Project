abb81a812f41a18c1f54bb8a9df0817e36369dcf
removedporn <86824510+removedporn@users.noreply.github.com>
2021-07-26 05:29:36 +0800

    Delete file directory

--- file/bsconfig.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  server: {
-    baseDir: ['example', '.'],
-    index: 'index.html'
-  },
-  open: false,
-  files: ['dist/*.js', 'example/*.js', 'example/*.css', 'example/*.html']
-}
